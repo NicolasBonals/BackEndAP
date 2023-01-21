d7ff2bb65c5687ff8ef94d6995c33e06bb55e1fe
NicolasBonals <nicobonals@gmail.com>
2023-01-20 23:51:37 -0300

    Backend

---
+++ src/main/java/com/portfolionab/nab/Security/Dto/LoginUsuario.java
@@ -0,0 +1,32 @@
+
+package com.portfolionab.nab.Security.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginUsuario {
+    @NotBlank //para que no esten vacios
+    private String nombreUsuario;
+    @NotBlank
+    private String password;
+    
+
+    
+    //Getters&Setters
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
---
+++ src/main/java/com/portfolionab/nab/Security/Enums/RolNombre.java
@@ -0,0 +1,5 @@
+package com.portfolionab.nab.Security.Enums;
+
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER;
+}
